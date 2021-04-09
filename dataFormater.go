cf74cfb653317572035061389e1bb0cab3420528
高鹏飞 <gaopengfei@soyoung.com>
2021-04-09 21:53:32 +0800

    数据格式化(1/2)

--- dataFormater.go
+++ dataFormater.go
@@ -1,7 +1,13 @@
 package risk
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
 func Text() {
@@ -9,6 +15,122 @@ func Text() {
 }
 
 type RiskData struct {
-	rawData       map[string]interface{}
-	formattedData map[string]interface{}
+	rawData    rawType
+	config     map[string]ConfigJsonTpl
+	intType    map[string]int
+	stringType map[string]string
+}
+
+// 原始数据类型
+type rawType map[string]interface{}
+type IntType int
+type StringType string
+
+// ConfigJsonTpl 字段配置 json 模板
+type ConfigJsonTpl struct {
+	KeyName  string        `json:"key_name"`
+	FuncName string        `json:"func_name"`
+	DataType string        `json:"type"`
+	Args     []interface{} `json:"args"`
+}
+
+// LoadConfig 加载配置
+func (rd *RiskData) LoadConfig(configJson string) error {
+	cf := []ConfigJsonTpl{}
+	err := json.Unmarshal([]byte(configJson), &cf)
+
+	if err != nil {
+		return err
+	}
+
+	rd.config = make(map[string]ConfigJsonTpl, len(cf))
+	for i := 0; i < len(cf); i++ {
+		rd.config[cf[i].KeyName] = cf[i]
+	}
+	return nil
+}
+
+// JsonDecode 通用的 json 解析方法
+func JsonDecode(raw string, tpl interface{}, useNumber bool) error {
+	if useNumber {
+		d := json.NewDecoder(bytes.NewReader([]byte(raw)))
+		d.UseNumber()
+		return d.Decode(tpl)
+	}
+
+	return json.Unmarshal([]byte(raw), &tpl)
+}
+
+// LoadData 加载数据
+func (rd *RiskData) LoadData(raw string) (err error) {
+
+	tpl := rawType{}
+
+	err = JsonDecode(raw, &tpl, true)
+	if err != nil {
+		return err
+	}
+
+	rd.rawData = make(rawType, len(tpl))
+	rd.rawData = tpl
+
+	log.Printf("%#+v \n", tpl)
+	err = rd.swithcType()
+	if err != nil {
+		return err
+	}
+
+	// log.Println(rd)
+
+	return nil
+}
+
+func dd(params ...interface{}) {
+	log.Println(params...)
+	os.Exit(0)
+}
+
+func (rd *RiskData) swithcType() (err error) {
+	if rd.config == nil {
+		return errors.New("未加载字段配置")
+	}
+
+	rd.intType = make(map[string]int, 1)
+	rd.stringType = make(map[string]string, 1)
+
+LOOP:
+	for k, kConfig := range rd.config {
+		rawValue, exist := rd.rawData[k]
+
+		if !exist {
+			continue LOOP
+		}
+
+		switch kConfig.DataType {
+		case "int":
+			str, ok := rawValue.(string)
+			var intValue int
+			if ok {
+				intValue, err = strconv.Atoi(str)
+			} else {
+				str := rawValue.(json.Number).String()
+				intValue, err = strconv.Atoi(str)
+				if err != nil {
+					continue LOOP
+				}
+			}
+			rd.intType[k] = intValue
+			break
+		case "string":
+			strValue, ok := rawValue.(string)
+			if ok {
+				rd.stringType[k] = strValue
+			}
+			break
+		default:
+			continue LOOP
+		}
+	}
+
+	return
 }
--- dataFormater_test.go
+++ dataFormater_test.go
@@ -4,6 +4,530 @@ import (
 	"testing"
 )
 
-func TestDemo(t *testing.T){
+// 应用配置
+var appConfig string = `
+{
+    "column_config": [
+        {
+            "key_name": "deduct_type",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "ip3",
+            "func_name": "subIp",
+            "args": [
+                "ip",
+                0,
+                "3"
+            ]
+        },
+        {
+            "key_name": "referer",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "os",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "pack_name",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "website",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "token",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "ext_id",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "androidid",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "oaid",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "idfa",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "imei",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "mac",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "create_time_ms",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "create_date",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "province_id",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "city_id",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "user_agent",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "change_point",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "ip_type",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "ip",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "ad_source",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "ad_type",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "content_type",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "product_price",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "item3",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "item2",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "item1",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "content_id",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "hos_city",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "hos_province",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "hospital_id",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "gps",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "device_type",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "device_id",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "msg_id",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "order_money",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "order_id",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "channel_name",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "sys",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "register_date",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        },
+        {
+            "key_name": "uid",
+            "func_name": "",
+			"type" :"string",
+            "args": null
+        }
+    ],
+    "strategy_id_list": [
+        "123",
+        "124",
+        "125",
+        "126",
+        "127",
+        "128",
+        "129",
+        "130",
+        "131",
+        "132",
+        "133",
+        "134",
+        "135",
+        "137",
+        "138",
+        "139",
+        "140",
+        "141",
+        "142",
+        "143",
+        "144",
+        "145",
+        "146",
+        "147",
+        "148",
+        "149",
+        "150",
+        "151",
+        "152",
+        "153",
+        "154",
+        "155",
+        "156",
+        "157",
+        "158",
+        "159",
+        "160",
+        "161",
+        "162",
+        "165"
+    ],
+    "rule_config": [
+        {
+            "rule_id": "16",
+            "app_id": "4"
+        },
+        {
+            "rule_id": "15",
+            "app_id": "4"
+        },
+        {
+            "rule_id": "21",
+            "app_id": "4"
+        }
+    ],
+    "version": "v2.1"
+}
+`
+
+// 策略配置
+var StrategyConfig string = `
+{
+    "id": 16,
+    "app_id": 4,
+    "weight": 4,
+    "trigger": 8,
+    "status": 1,
+    "rule_filter": {
+        "theme": "any",
+        "rule": [
+            {
+                "rule_id": 57
+            },
+            {
+                "rule_id": 56
+            }
+        ]
+    }
+}
+`
+
+// 字段配置
+var DataFormatterConfig string = `
+[
+    {
+        "key_name": "ip3",
+        "func_name": "subIp",
+        "args": [
+            "ip",
+            0,
+            "3"
+        ],
+        "type": "string"
+    },
+    {
+        "key_name": "ip",
+        "func_name": "",
+        "args": null,
+        "type": "string"
+    },
+    {
+        "key_name": "hospital_id",
+        "func_name": "",
+        "type": "int",
+        "args": null
+    },
+    {
+        "key_name": "create_time_ms",
+        "func_name": "",
+        "type": "int",
+        "args": null
+    },
+    {
+        "key_name": "create_date",
+        "func_name": "",
+        "type": "int",
+        "args": null
+    },
+    {
+        "key_name": "uid",
+        "func_name": "",
+        "type": "int",
+        "args": null
+    }
+]
+`
+
+// 简单指标原子配置
+var SimpleQuotaConfig string = `
+{
+    "strategy_atom_id": 163,
+    "condition": "count",
+    "search_key": [
+        "hospital_id"
+    ],
+    "count_key": "uid",
+    "system_sign": 0,
+    "version": "v2.1",
+    "app_id": 104,
+    "time_shard": 86400000,
+    "filter": {
+        "theme": "any",
+        "rule": [
+            {
+                "source": "uid",
+                "condition": "notEmpty",
+                "target": 11111
+            }
+        ]
+    }
+}
+`
+
+// 复合指标配置
+var MultiQuotaConfig string = `
+{
+    "complex_quota_id": 35,
+    "system_sign": 0,
+    "version": "v2.1",
+    "app_id": 107,
+    "filter": [],
+    "formula": "#189\/#181",
+    "quota_map": {
+        "#181": {
+            "app_id": 107,
+            "strategy_id": 181
+        },
+        "#189": {
+            "app_id": 107,
+            "strategy_id": 189
+        }
+    }
+}
+`
+
+// 规则配置
+var ruleConfig string = `
+{
+    "rule_id": 94,
+    "warning_point": 0.2,
+    "forbidden_point": 0.5,
+    "filter": {
+        "theme": "",
+        "rule": [
+            {
+                "source": "",
+                "condition": "",
+                "target": ""
+            }
+        ]
+    },
+    "strategy_config": {
+        "id": 35,
+        "type": "multi"
+    }
+}
+`
+
+var RawData string = `
+{
+	"uid": 14625461,
+	"ad_type": 21,
+	"sys": 1,
+	"device_id": 203310581,
+	"hospital_id": "108720",
+	"content_id": 597322,
+	"content_type": 1,
+	"ip": "61.50.119.46",
+	"change_point": 5,
+	"create_date": "2021-04-09 15:06:20",
+	"province_id": 1,
+	"city_id": 1,
+	"website": "app",
+	"ip2city": "1",
+	"ip2province": "1",
+	"ext_id": "174127",
+	"deduct_type": 1
+}
+`
+
+func TestDemo(t *testing.T) {
 	Text()
+}
+
+func TestConfig(t *testing.T) {
+	t.Log("start")
+	rd := RiskData{}
+	err := rd.LoadConfig(DataFormatterConfig)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(rd)
+	t.Log("end")
+}
+
+func TestData(t *testing.T) {
+	t.Log("start")
+	rd := RiskData{}
+	err := rd.LoadConfig(DataFormatterConfig)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = rd.LoadData(RawData)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(rd)
+	t.Log("end")
+}
+
+func TestJsonDecode(t *testing.T) {
+	t.Log("start")
+	data := `
+{
+    "uid": 14625461,
+    "ad_type": 21,
+    "sys": 1,
+    "hospital_id": "108720"
+}
+`
+	tpl := map[string]interface{}{}
+	JsonDecode(data, &tpl, true)
+	t.Logf("%#+v \n", tpl)
 }
